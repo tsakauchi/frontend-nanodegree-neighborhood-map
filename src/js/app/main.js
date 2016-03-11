5436ffe5e675d7cfee3c05dccf03c39553d9089f
Tsuginosuke Sakauchi <tsuginosuke.sakauchi@fairlandcircle.com>
2016-03-11 14:08:44 -0600

    feat: Add Google Maps to my app

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -3,12 +3,19 @@ define([
   "jquery",
   "underscore",
   "knockout",
+  "googlemaps",
   "app/viewmodel/Locations"
-], function(c,$,_,ko,Locations) {
+], function(c,$,_,ko,gmaps,Locations) {
   $(function() {
     "use strict";
 
     var locations = ko.utils.parseJson(window.localStorage.getItem(c.LOCAL_STORAGE_ITEM_KEY));
     ko.applyBindings(new Locations(locations || []));
+
+    var map = new gmaps.Map(document.getElementById('map'), {
+      center: {lat: 43.05, lng: -87.96},
+      zoom: 8
+    });
+
   });
 });
