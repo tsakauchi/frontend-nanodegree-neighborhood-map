abd49875d3b4621235a51b9d8dea1f8c2654d230
Tsuginosuke Sakauchi <mobius@fairlandcircle.com>
2016-03-13 11:49:42 -0500

    feat: Add Google Maps marker when adding location to list

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -5,17 +5,19 @@ define([
   "knockout",
   "googlemaps",
   "app/viewmodel/Locations"
-], function(c,$,_,ko,gmaps,Locations) {
-  $(function() {
-    "use strict";
+], function(c, $, _, ko, gmaps, Locations) {
+  "use strict";
 
-    var locations = ko.utils.parseJson(window.localStorage.getItem(c.LOCAL_STORAGE_ITEM_KEY));
-    ko.applyBindings(new Locations(locations || []));
-
-    var map = new gmaps.Map(document.getElementById('map'), {
-      center: {lat: 43.05, lng: -87.96},
-      zoom: 8
-    });
-
+  var map = new gmaps.Map(document.getElementById('map'), {
+    center: {lat: 43.05, lng: -87.96},
+    zoom: 8
   });
+
+  var locations = ko.utils.parseJson(window.localStorage.getItem(c.LOCAL_STORAGE_ITEM_KEY));
+  ko.applyBindings(new Locations(locations || [], map));
+
+
+
+
+
 });
--- src/js/app/viewmodel/Locations.js
+++ src/js/app/viewmodel/Locations.js
@@ -1,24 +1,68 @@
 define([
   "app/globals",
+  "jquery",
+  "underscore",
   "knockout",
+  "googlemaps",
   "app/model/Location"
-], function(c, ko, Location) {
+], function(c, $, _, ko, gmaps, Location) {
   "use strict";
 
-  return function Locations(locations) {
+  return function Locations(locations, map) {
     var self = this;
 
+    self.map = map;
+
+    self.geocoder = new gmaps.Geocoder();
+
     self.locations = ko.observableArray(ko.utils.arrayMap(locations, function (location) {
-      return new Location(location.name);
+      return new Location(location.name || {});
     }));
 
-    self.add = function () {
-      self.locations.push(new Location("Location 1"));
+    // non-persisted marker array
+    self.markers = {};
+
+    self.addMarker = function(name) {
+      self.geocoder.geocode({ address: name }, function(results, status) {
+        if (status == gmaps.GeocoderStatus.OK) {
+          self.map.setCenter(results[0].geometry.location);
+          self.markers[name] = new gmaps.Marker({
+            map: self.map,
+            position: results[0].geometry.location
+          });
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
     };
 
-    self.remove = function (location) {
+    self.addLocation = function(name) {
+      self.locations.push(new Location(name));
+    };
+
+    self.submitEventHandler = function(formElement) {
+      var $form = $(formElement);
+      var $locn = $form.find("#location");
+      var name = $locn.val();
+
+      self.addLocation(name);
+      self.addMarker(name);
+
+      $locn.val("");
+    };
+
+    self.remove = function(location) {
+      if (self.markers[location.name()]) {
+        self.markers[location.name()].setMap(null);
+        delete self.markers[location.name()];
+      }
       self.locations.remove(location);
     };
+
+    // initialize internal marker collection
+    ko.utils.arrayForEach(self.locations(), function(location) {
+      self.addMarker(location.name());
+    });
 
     // internal computed observable that fires whenever anything changes in Locations
     ko.computed(function () {
